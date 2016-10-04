4e6446320b5f171fe8b5f6cfe462c8194de46b7d
gammy <gammy@users.noreply.github.com>
2016-10-04 02:45:21 +0200

    Add another option: go to the oldest archived copy. Renamed the submenu items

--- wayback_helper.js
+++ wayback_helper.js
@@ -1,14 +1,20 @@
 var wayback_machine_url = 'https://web.archive.org/web';
 
 chrome.contextMenus.create({
-    id: 'wayback-get-latest',
-    title: 'Get the latest archived copy',
+    id: 'wayback-get-newest',
+    title: 'Go to newest',
+    contexts: ['all']
+});
+
+chrome.contextMenus.create({
+    id: 'wayback-get-oldest',
+    title: 'Go to oldest',
     contexts: ['all']
 });
 
 chrome.contextMenus.create({
     id: 'wayback-search',
-    title: 'Search for archived copies',
+    title: 'Search the archive',
     contexts: ['all']
 });
 
@@ -20,7 +26,14 @@ function waybackSearch(tab) {
     chrome.tabs.update(tab.id, {url: wayback_url});
 }
 
-function waybackLatest(tab) {
+function waybackOldest(tab) {
+    console.log('Looking up the earliest archive');
+    var wayback_url = wayback_machine_url + '/0/' + tab.url;
+    console.log('> ' + wayback_url);
+    chrome.tabs.update(tab.id, {url: wayback_url});
+}
+
+function waybackNewest(tab) {
     console.log('Looking up the newest archive');
     // Try to access a page whose date is the first day of next year
     var date = new Date();
@@ -35,8 +48,11 @@ function contextClick(info, tab) {
         case('wayback-search'):
             waybackSearch(tab);
             break;
-        case('wayback-get-latest'):
-            waybackLatest(tab);
+        case('wayback-get-oldest'):
+            waybackOldest(tab);
+            break;
+        case('wayback-get-newest'):
+            waybackNewest(tab);
             break;
     }
 }
